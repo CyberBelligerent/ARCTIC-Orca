1889fcb56187c76e3eda138606c947e0c9136d8b
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-01-25 21:42:08 +0100

    Security Readded
    Security Framework has been readded:
    
    - Enabled Web Security
    - Added JWT and CookieFilter for Authentication and Authorization
    - Readded Filter chain

--- src/main/java/com/rahman/arctic/orca/Orca.java
+++ src/main/java/com/rahman/arctic/orca/Orca.java
@@ -1,16 +1,78 @@
 package com.rahman.arctic.orca;
 
+import java.security.SecureRandom;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.rahman.arctic.orca.filters.CookieFilter;
+import com.rahman.arctic.orca.filters.JwtRequestFilter;
+import com.rahman.arctic.orca.utils.IUserService;
 
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.rahman.arctic.orca.repos"})
 @EntityScan("com.rahman.arctic.orca.objects")
+@EnableWebSecurity
 public class Orca {
 
 	public Orca() {
 		System.out.println("Enabling Service: Orca");
+	}
+	
+	@Autowired
+	private JwtRequestFilter jwtFilter;
+
+	@Autowired
+	private CookieFilter cookieFilter;
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new IUserService();
+	}
+
+	@Bean
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(15, new SecureRandom());
+	}
+
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		return authProvider;
+	}
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf ->
+				csrf.disable()
+			)
+			.authorizeHttpRequests(authorizeRequests -> 
+				authorizeRequests
+					.requestMatchers(HttpMethod.POST, "/range-api/v1/regularUser").hasAnyAuthority("ADMIN", "USER")
+					.requestMatchers("/**").permitAll()
+					.anyRequest().authenticated()
+			).sessionManagement(session -> 
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			);
+		
+		http.addFilterBefore(cookieFilter, UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
 	}
 	
 }
